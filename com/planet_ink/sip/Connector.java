bdb29749cd67f3ad0d3cb963a69eed2c46ee2dea
bozimmerman <bo@zimmers.net>
2015-09-09 02:53:48 +0000

    git-svn-id: svn://192.168.1.10/public/Sip@12985 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/sip/Connector.java
+++ com/planet_ink/sip/Connector.java
@@ -7,139 +7,144 @@
 package com.planet_ink.sip;
 
 import java.io.*;
-import java.util.*;
-import java.sql.*;
 import java.net.*;
+import java.nio.ByteBuffer;
+
 /**
  *
  * @author  Bo_Zimmerman
  * @version 
  */
-public class Connector extends Thread {
+public class Connector extends Thread 
+{
+	public static final int SOTIMEOUT	= 300;
 
-    public boolean connected=false;
-    public Socket sock=null;
-    public BufferedReader in;
-    public PrintWriter out;
-    public Sip myApp=null;
-    public StringBuffer input=new StringBuffer("");
-    public static final int SOTIMEOUT=300;
-    /** Creates new Connector */
-    public Connector() {
-        
-    }
+	public boolean 			connected	= false;
+	public Socket 			sock		= null;
+	public BufferedReader	in;
+	public PrintWriter 		out;
+	public Sip 				myApp		= null;
+	public StringBuffer 	input		= new StringBuffer("");
+	private ByteBuffer		ansiBuf		= ByteBuffer.allocate(4096);
+	
+	/** Creates new Connector */
+	public Connector() 
+	{
+		
+	}
 
-    public void sendOut(String output)
-    {
-        if(output==null) return;
-        out.print(output);
-        out.flush();
-    }
-    
-    public String connect(Sip A, String site, int port){
-        try
-        {
-            myApp=A;
-            sock=new Socket(site,port);
-            if(sock!=null)
-            {
-                sock.setSoTimeout(SOTIMEOUT);
-                out=new PrintWriter(sock.getOutputStream());
-                in=new BufferedReader(new InputStreamReader(sock.getInputStream()));
-            }
-            connected=true;
-        }
-        catch(Exception e)
-        {
-            connected=false;
-            return e.getMessage();
-        }
-        return "";
-    }
-    
-    public void disconnect()
-    {
-        connected=false;
-        try
-        {
-            if(in!=null)
-                in.close();
-            if(out!=null)
-                out.close();
-            if(sock!=null)
-                sock.close();
-        }
-        catch(Exception e)
-        {
-        }
-        in=null;
-        out=null;
-        sock=null;
-    }
-    public void run() 
-    {
-            StringBuffer tout=new StringBuffer("");
-            try
-            {
-                while(true)
-                {
-                    try
-                    {
-                        char c=(char)in.read();
-                        if((c>0)&&(c!=13))
-                        {
-                            tout.append(c);
-                            if(c==65535)
-                            {
-                                disconnect();
-                                myApp.lilboy();
-                                return;
-                            }
-                            if((tout.length()>100)||(c==10))
-                            {
-                                myApp.writeOut(tout.toString());
-                                tout.setLength(0);
-                            }
-                        }
-						
-                    }
-                    catch(java.io.InterruptedIOException e)
-                    {
-						if(e.getMessage().toUpperCase().indexOf("CONNECTION RESET BY PEER")>=0)
-						{
-							disconnect();
-							myApp.lilboy();
-							return;
-						}
-						else
-                        if(tout.length()>0)
-                        {
-                            myApp.writeOut(tout.toString());
-                            tout.setLength(0);
-                        }
-                        
-                    }
-                    catch(Exception e)
-                    {
-						if(e.getMessage().toUpperCase().indexOf("CONNECTION RESET BY PEER")>=0)
-						{
-							disconnect();
-							myApp.lilboy();
-							return;
-						}
-                        break;
-                    }
-                }
-            }
-            catch(Exception e)
-            {
-				if(e.getMessage().toUpperCase().indexOf("CONNECTION RESET BY PEER")>=0)
+	public void sendOut(String output)
+	{
+		if(output==null) return;
+		out.print(output);
+		out.flush();
+	}
+	
+	public String connect(Sip A, String site, int port)
+	{
+		try
+		{
+			myApp=A;
+			sock=new Socket(site,port);
+			if(sock!=null)
+			{
+				sock.setSoTimeout(SOTIMEOUT);
+				out=new PrintWriter(sock.getOutputStream());
+				in=new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			}
+			connected=true;
+		}
+		catch(Exception e)
+		{
+			connected=false;
+			return e.getMessage();
+		}
+		return "";
+	}
+	
+	public void disconnect()
+	{
+		connected=false;
+		try
+		{
+			if(in!=null)
+				in.close();
+			if(out!=null)
+				out.close();
+			if(sock!=null)
+				sock.close();
+		}
+		catch(Exception e)
+		{
+		}
+		in=null;
+		out=null;
+		sock=null;
+	}
+	public void run() 
+	{
+		StringBuffer tout=new StringBuffer("");
+		try
+		{
+			while(true)
+			{
+				try
 				{
-					disconnect();
-					myApp.lilboy();
-					return;
+					char c=(char)in.read();
+					if((c>0)&&(c!=13))
+					{
+						tout.append(c);
+						if(c==65535)
+						{
+							disconnect();
+							myApp.lilboy();
+							return;
+						}
+						if((tout.length()>100)||(c==10))
+						{
+							myApp.writeOut(tout.toString());
+							tout.setLength(0);
+						}
+					}
+					
 				}
-                e.printStackTrace();
-            }
-    }
+				catch(java.io.InterruptedIOException e)
+				{
+					if(e.getMessage().toUpperCase().indexOf("CONNECTION RESET BY PEER")>=0)
+					{
+						disconnect();
+						myApp.lilboy();
+						return;
+					}
+					else
+					if(tout.length()>0)
+					{
+						myApp.writeOut(tout.toString());
+						tout.setLength(0);
+					}
+					
+				}
+				catch(Exception e)
+				{
+					if(e.getMessage().toUpperCase().indexOf("CONNECTION RESET BY PEER")>=0)
+					{
+						disconnect();
+						myApp.lilboy();
+						return;
+					}
+					break;
+				}
+			}
+		}
+		catch(Exception e)
+		{
+			if(e.getMessage().toUpperCase().indexOf("CONNECTION RESET BY PEER")>=0)
+			{
+				disconnect();
+				myApp.lilboy();
+				return;
+			}
+			e.printStackTrace();
+		}
+	}
 }
--- com/planet_ink/sip/MUDPain.java
+++ com/planet_ink/sip/MUDPain.java
@@ -3,18 +3,21 @@ package com.planet_ink.sip;
 import javax.swing.JTextPane;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
+//import javax.swing.text.html.*;
 
 public class MUDPain extends JTextPane
 {
-
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -7120722857208552965L;
 
+	//private HTMLEditorKit htmlEdKit = new HTMLEditorKit();
+	
 	public MUDPain() 
 	{
 		super();
+		//this.setEditorKit(htmlEdKit);
 	}
 	
 	public void append(String str) 
