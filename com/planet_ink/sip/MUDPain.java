16fb6a58c335ac4ce32e59b5137bb31f1c1ea800
bozimmerman <bo@zimmers.net>
2020-02-27 07:17:37 +0000

    Switching to an editor kit.  Siplet integration forthcoming.
    git-svn-id: svn://192.168.1.10/public/Sip@19202 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/sip/MUDPain.java
+++ com/planet_ink/sip/MUDPain.java
@@ -1,23 +1,30 @@
 package com.planet_ink.sip;
 
+import javax.swing.JEditorPane;
 import javax.swing.JTextPane;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
-//import javax.swing.text.html.*;
+import javax.swing.text.html.*;
 
-public class MUDPain extends JTextPane
+public class MUDPain extends JEditorPane 
 {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -7120722857208552965L;
 
-	//private HTMLEditorKit htmlEdKit = new HTMLEditorKit();
+	private HTMLEditorKit htmlEdKit = new HTMLEditorKit();
 	
 	public MUDPain() 
 	{
 		super();
-		//this.setEditorKit(htmlEdKit);
+		this.setEditorKit(htmlEdKit);
+		this.setEditable(false);
+		StyleSheet styleSheet = htmlEdKit.getStyleSheet();
+        styleSheet.addRule("body {color:#000; font-family:monospace; margin: 4px; }");
+        styleSheet.addRule("h1 {color: blue;}");
+        styleSheet.addRule("h2 {color: #ff0000;}");
+        styleSheet.addRule("pre {font : 10px monospace; color : black; background-color : #fafafa; }");
 	}
 	
 	public void append(String str) 
