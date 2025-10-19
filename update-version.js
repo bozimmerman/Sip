1b469e77ff650e21e153112f5e921ec8f5d530bb
bozimmerman <bo@zimmers.net>
2025-10-19 20:36:26 +0000

    git-svn-id: svn://192.168.1.10/public/Sip@24686 0d6f1817-ed0e-0410-87c9-987e46238f29

--- update-version.js
+++ update-version.js
@@ -23,15 +23,44 @@ const fullVersion = `${major}.${minor}`;  // Outputs "3.2.1" based on your examp
 var variation = 'a';
 if (process.argv.length > 2)
 	variation =  process.argv[2];
-var appName = 'Sip MUD Client';
-var pkgAppName = 'Sip';
-if (variation === 'b')
+// a: windows/mac Sip,
+// b: windows/map Sip Of CoffeeMud
+// c: linux Sip
+// d: linux Sip of CoffeeMud
+var cmOnlyFlag = (variation === 'b') ? 'true' : 'false';
+var productName = 'Sip MUD Client';
+var topName = 'Sip';
+var artifactName = 'Sip';
+var displayName = 'Sip MUD Client';
+var description = 'Sip -a MUD Client!'
+if(variation === 'a')
+{}
+if(variation === 'b')
 {
-	appName = 'Sip of CoffeeMud';
-	pkgAppName = 'SipOfCoffeemud';
+	productName = 'Sip of CoffeeMud';
+	displayName = 'Sip of CoffeeMud';
+	topName = 'SipOfCoffeemud';
+	artifactName = 'SipOfCoffeeMud'
+	description = "Sip of CoffeeMud - A MUD client optimized for CoffeeMud";
 }
 else 
-if (variation !== 'a')
+if(variation === 'c')
+{
+	productName = 'sip-mud-client';
+	displayName = 'Sip MUD Client';
+	topName = 'sip-mud-client';
+	artifactName = 'Sip'
+}
+else 
+if(variation === 'b')
+{
+	productName = 'sip-of-coffeemud';
+	displayName = 'Sip of CoffeeMud';
+	topName = 'sip-of-coffeemud';
+	artifactName = 'SipOfCoffeeMud'
+	description = "Sip of CoffeeMud - A MUD client optimized for CoffeeMud";
+}
+else 
 {
 	console.error('Error: Invalid variation argument. Must be "a" or "b".');
 	process.exit(1);
@@ -39,25 +68,22 @@ if (variation !== 'a')
 
 const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
 pkg.version = fullVersion;
-
-if(variation === 'b')
-	pkg.description = "Sip of CoffeeMud - A MUD client optimized for CoffeeMud";
-else
-	pkg.description = "Sip - A MUD Client!";
-pkg.name = pkgAppName;
-pkg.build.productName = appName;
+pkg.description = description;
+pkg.name = topName;
+pkg.build.productName = productName;
+pkg.build.linux.executeableName = productName;
+pkg.build.linux.artifactName = pkg.build.linux.artifactName.replace('${name}', artifactName);
+pkg.build.linux.desktop.Comment = description;
+pkg.build.linux.desktop.Name = displayName;
+  
 fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 4) + '\n');  // Preserve formatting
 
 console.log(`Updated package.json version to ${fullVersion}`);
 
-var variation = 'a';
-if (process.argv.length > 2)
-	variation =  process.argv[2];
-
 const nameRegex = /NAME:\s*window\.isElectron\?'([^']+?)':'Siplet',/;
-var newJsContent = jsContent.replace(nameRegex, "NAME: window.isElectron?'"+appName+"':'Siplet',");
+var newJsContent = jsContent.replace(nameRegex, "NAME: window.isElectron?'"+displayName+"':'Siplet',");
 const cmRegex = /COFFEE_MUD:\s*(true|false),/;
-newJsContent = newJsContent.replace(cmRegex, `COFFEE_MUD: ${variation === 'b' ? 'true' : 'false'},`);
+newJsContent = newJsContent.replace(cmRegex, `COFFEE_MUD: ${cmOnlyFlag},`);
 fs.writeFileSync(jsPath, newJsContent);
 console.log(`Updated Siplet.NAME to '${appName}'`);
 
